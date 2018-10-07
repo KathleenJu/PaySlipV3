c9ebf2c1ab2701934d85d081e0a1a0076f500063
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-08 09:49:50 +1300

    added tests for tax calculations

---
+++ PaySlip/PaySlip/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaySlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
