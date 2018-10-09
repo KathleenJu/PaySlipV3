606a721c15aec7627837fe9b4a1695cd4b1c36d8
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-10-10 07:47:40 +1300

    added input and output json files, getEmployeeDetails methods for consoleReader

---
+++ PaySlip/PaySlip/Input/ConsoleInputReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaySlip.Input
+{
+    public class ConsoleInputReader : IInputReader
+    {
+        public Dictionary<string, string> GetEmployeeDetails(Dictionary<string, string> inputFields)
+        {
+            var employeeDetails = new Dictionary<string, string>();
+            Console.Write("Welcome to the payslip generator! \n");
+            foreach (var field in inputFields)
+            {
+                Console.Write(field.Value);
+                employeeDetails.Add(field.Key, Console.ReadLine());
+            }
+
+            return employeeDetails;
+        }
+    }
+}
---
+++ PaySlip/PaySlip/Input/IInputReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PaySlip.Input
+{
+    public interface IInputReader
+    {
+        Dictionary<string, string> GetEmployeeDetails(Dictionary<string, string> inputFields);
+        
+    }
+}
